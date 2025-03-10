28e6ce3462debdb771bf6408efc69e4bed1581f9
dan <danielkarongoi@gmail.com>
2025-03-10 05:24:56 +0300

    added changes

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // Should equal 62
 
-// Write your code here
+// Random
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer > 0
+
+// Modulus
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // Should equal 4
+
+// Max
+let max = Math.max(5, 10, 15, 20); // Should return 20
